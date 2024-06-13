8fb2e3c4b3ac02f5ebbc13cf3ee588297a24a280
ABHISHEK-PRASAD-BAHELIYA <singhabhishek74401@gmail.com>
2024-06-13 17:11:02 +0530

    Finish sudoku & now you Play :)

--- Backtracking/SudokuGame.java
+++ Backtracking/SudokuGame.java
@@ -1,7 +1,37 @@
 public class SudokuGame {
+    public static boolean isSafe(int sudoku[][], int row, int col, int digit) {
+        // column
+        for (int i = 0; i <= 8; i++) {
+            if (sudoku[i][col] == digit) {
+                return false;
+            }
+        }
+
+        // row
+        for (int j = 0; j <= 8; j++) {
+            if (sudoku[row][j] == digit) {
+                return false;
+            }
+        }
+
+        // grid
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+        // 3x3
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (sudoku[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     public static boolean sudokuSolver(int sudoku[][], int row, int col) {
         // base case
-        if (row == 9 && col == 9) {
+        if (row == 9 && col == 0) {
             return true;
         } else if (row == 9) {
             return false;
@@ -30,6 +60,15 @@ public class SudokuGame {
         return false;
     }
 
+    public static void printSudoku(int sudoku[][]) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(sudoku[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         int sudoku[][] = { { 0, 0, 8, 0, 0, 0, 0, 0, 0 },
                 { 4, 9, 0, 1, 5, 7, 0, 0, 2 },
@@ -40,6 +79,11 @@ public class SudokuGame {
                 { 0, 3, 0, 0, 7, 2, 0, 0, 4 },
                 { 0, 4, 9, 0, 3, 0, 0, 5, 7 },
                 { 8, 2, 7, 0, 0, 9, 0, 1, 3 } };
-
+        if (sudokuSolver(sudoku, 0, 0)) {
+            System.out.println("Solution exists");
+            printSudoku(sudoku);
+        } else {
+            System.out.println("Solutions doesn't exists");
+        }
     }
 }
